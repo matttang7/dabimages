a7bdca1fd48819ca87feaa9a7c2c2eb255603285
matttang7 <mt13@illinois.edu>
2020-04-22 18:54:30 -0500

    fixing problem with message substring

--- bot.js
+++ bot.js
@@ -22,42 +22,43 @@ var files = fs.readdirSync(__dirname + '/dabimages')
 bot.on('message', async message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        if(cmd.substring(0,3) == 'dab' | cmd.substring(0,3) == 'Dab'){
-            let num = parseInt(args)/files.length
-            console.log(typeof num === 'number')
-            if(typeof num === 'number' && !isNaN(num)){
-                let chosenFile = files[Math.floor(num * files.length)] 
-                bot.uploadFile({
-                    to: channelID,
-                    file: 'dabimages/' + chosenFile
-                });
-            }
-            else{
-                let chosenFile = files[Math.floor(Math.random() * files.length)] 
-                bot.uploadFile({
-                    to: channelID,
-                    file: 'dabimages/' + chosenFile
-                });
-            }
-        }
-        else if(cmd.substring(0,4) == 'help'){
-            bot.sendMessage({
+    if(message.author.bot) return;
+
+    if(message.content.indexOf('!') !== 0) return;
+
+    const args = message.content.slice(1).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    if(command === 'dab'){
+        let num = parseInt(args[0])/files.length
+        console.log(typeof num === 'number')
+        if(typeof num === 'number' && !isNaN(num)){
+            let chosenFile = files[Math.floor(num * files.length)] 
+            bot.uploadFile({
                 to: channelID,
-                message: 'To use this bot, send !dab for randomized dab picture or !dab (some number from 0 to 368) for a specific image'
+                file: 'dabimages/' + chosenFile
             });
         }
         else{
-            bot.sendMessage({
+            let chosenFile = files[Math.floor(Math.random() * files.length)] 
+            bot.uploadFile({
                 to: channelID,
-                message: 'I don\'t understand that command'
+                file: 'dabimages/' + chosenFile
             });
         }
-     }
+    }
+    else if(command === 'help'){
+        bot.sendMessage({
+            to: channelID,
+            message: 'To use this bot, send !dab for randomized dab picture or !dab (some number from 0 to 368) for a specific image'
+        });
+    }
+    else{
+        bot.sendMessage({
+            to: channelID,
+            message: 'I don\'t understand that command'
+        });
+    }
 });
 
 bot.login(process.env.token);
