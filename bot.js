ba1a236e7ce4d2e69751ee7e555d646aefc49eaf
matttang7 <mt13@illinois.edu>
2020-04-29 16:14:37 -0500

    fixed bug in check file

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ function checkFile(file, message) {
     if (typeof file !== "undefined") {
         message.channel.send("", {
             files: [
-                'dabimages/' + chosenFile
+                'dabimages/' + file
             ]
         });
     }
